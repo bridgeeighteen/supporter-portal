b4c775f7247125ea10e49279759c6032e2c0a14d
Diamochang <diamochang@yeah.net>
2026-07-24 20:27:23 +0800

    view: Better style

--- templates/header.php
+++ templates/header.php
@@ -13,7 +13,6 @@ $siteUrl  = Config::siteUrl();
     <title><?= htmlspecialchars($pageTitle ?? $siteName, ENT_QUOTES, 'UTF-8') ?></title>
     <meta name="description" content="十八桥社区支持者门户">
     <link rel="icon" href="/favicon.ico" sizes="any">
-    <link rel="icon" href="/assets/images/logo.svg" type="image/svg+xml">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
     <link rel="stylesheet" href="/assets/css/style.css">
