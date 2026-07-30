3a987bc1bfac59b611064b250639d0530b07bf20
Diamochang <diamochang@yeah.net>
2026-07-30 09:43:49 +0800

    fix: Typo in footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -4,7 +4,7 @@
             <p class="footer-statement">连接兴趣与文化的桥梁</p>
             <div class="footer-meta">
                 <p class="footer-text">
-                    Copyright &copy; 2026 The Bridge Eighteen Community.<br>j8                    <a href="https://codeberg.org/bridgeeighteen/supporter-portal" target="_blank" rel="noopener">Software</a> is licensed under AGPLv3 or any later version.
+                    Copyright &copy; 2026 The Bridge Eighteen Community.<br>                    <a href="https://codeberg.org/bridgeeighteen/supporter-portal" target="_blank" rel="noopener">Software</a> is licensed under AGPLv3 or any later version.
                 </p>
             </div>
         </div>
